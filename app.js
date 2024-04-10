4eb81b8e77b7dd8bbc7c9b842a733f07e6fbdb25
NDEBY Véronique Chimène <80843218+Shimouna-14@users.noreply.github.com>
2024-04-11 00:45:21 +0200

    fix api

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', process.env.HOST);
+  res.setHeader('Access-Control-Allow-Origin', process.env.API);
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
